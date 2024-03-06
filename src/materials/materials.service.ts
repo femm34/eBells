b73efa3e5d598194df2d7d0de23d56e7183c99b4
femm34 <femm15.mm@gmail.com>
2024-03-05 21:48:59 -0600

    the materials logical business was added

--- src/materials/materials.service.ts
+++ src/materials/materials.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { CreateMaterialDto } from './dto/create-material.dto';
 import { UpdateMaterialDto } from './dto/update-material.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Material } from './entities/material.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class MaterialsService {
-  create(createMaterialDto: CreateMaterialDto) {
-    return 'This action adds a new material';
+  constructor(
+    @InjectRepository(Material)
+    private materialRepository: Repository<Material>,
+  ) { }
+
+  async create(createMaterialDto: CreateMaterialDto) {
+    const newMaterial = this.materialRepository.create(createMaterialDto);
+    return await this.materialRepository.save(newMaterial);
   }
 
-  findAll() {
-    return `This action returns all materials`;
+  async findAll() {
+    return await this.materialRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} material`;
+  async findOne(id: number) {
+    return await this.materialRepository.findOne({ where: { id: id } })
   }
 
-  update(id: number, updateMaterialDto: UpdateMaterialDto) {
-    return `This action updates a #${id} material`;
+  async update(id: number, updateMaterialDto: UpdateMaterialDto) {
+    await this.materialRepository.update(id, updateMaterialDto)
+    return await this.materialRepository.findOne({ where: { id: id } })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} material`;
+  async remove(id: number) {
+    return await this.materialRepository.delete(id)
   }
 }
