7e8f87beb8c3b2d5e4cb713c9f895b2eae87869b
femm34 <femm15.mm@gmail.com>
2024-03-07 20:41:31 -0600

    cors is enabled

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,8 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('eBells docs')
     .setDescription('The eBells API description')
