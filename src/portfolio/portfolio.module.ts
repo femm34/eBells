63fe1a208990f5e681ae507b5e6d02c59dd6999a
femm34 <femm15.mm@gmail.com>
2024-03-17 11:10:08 -0600

    portfolio module was updated

--- src/portfolio/portfolio.module.ts
+++ src/portfolio/portfolio.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PortfolioService } from './portfolio.service';
 import { PortfolioController } from './portfolio.controller';
+import { Portfolio } from './entities/portfolio.entity';
+import { Role } from 'src/roles/entities/role.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Portfolio, Role])],
   controllers: [PortfolioController],
   providers: [PortfolioService],
 })
-export class PortfolioModule {}
+export class PortfolioModule { }
